77a4741beeca0f40dd54463be7973e14e467a8ac
Daniel Golden <dangold@bergen.org>
2015-10-10 16:48:32 -0700

    Initial Commit

---
+++ BitCoinViewer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BitCoinViewer
+//
+//  Created by Daniel Golden on 10/10/15.
+//  Copyright © 2015 AndrewDanPatrickCalHacks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
