dc6bae11114b1b35ac1d20d59e5c192cfac8b982
Daniel Golden <dangold@bergen.org>
2015-10-10 20:24:08 -0700

    hi andrew

--- BitCoinViewer/FirstViewController.swift
+++ BitCoinViewer/FirstViewController.swift
@@ -14,13 +14,18 @@ class FirstViewController: UITableViewController {
 
   override func viewDidLoad() {
     
-    let url = NSURL(string: "https://blockchain.info/ticker")
+    let url:NSURL = NSURL(string: "https://blockchain.info/ticker")!
+    let session = NSURLSession.sharedSession()
+    
+    let request = NSMutableURLRequest(URL: url)
+    request.HTTPMethod = "GET"
 
-    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-      if(data?.length > 0 && error == nil) {
+    let task = session.dataTaskWithRequest(request) { (let data, let response, let error) in
+      dispatch_async(dispatch_get_main_queue(), {
         let json = NSString(data: data!, encoding: NSASCIIStringEncoding)
-        parse_json(json);
-      }
+        self.parse_json(json!)
+        return
+      })
     }
     
     task.resume()
@@ -36,16 +41,22 @@ class FirstViewController: UITableViewController {
   
   func parse_json(data:NSString) {
     let jsonData:NSData = data.dataUsingEncoding(NSUTF8StringEncoding)!
-    let json: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions())
-    if let currencies = json as? NSArray {
+    let json: AnyObject?
+    do {
+      json = try NSJSONSerialization.JSONObjectWithData(jsonData, options: [])
+    } catch let error as NSError {
+      json = nil
+      print("error")
+    }
+    if let currencies = json as? NSDictionary {
       for (var i = 0; i < currencies.count ; i++ ) {
         if let currency_object = currencies[i] as? NSDictionary {
-          if let fifteenM = currencies["15m"] as? String {
-            if let last = currencies["last"] as? String {
-              if let buy = currencies["buy"] as? String {
-                if let sell = currencies["sell"] as? String {
-                  if let symbol = currencies["symbol"] as? String {
-                    
+          if let fifteenM = currency_object["15m"] as? String {
+            if let last = currency_object["last"] as? String {
+              if let buy = currency_object["buy"] as? String {
+                if let sell = currency_object["sell"] as? String {
+                  if let symbol = currency_object["symbol"] as? String {
+                    TableData.append(fifteenM)
                   }
                 }
               }
@@ -56,7 +67,14 @@ class FirstViewController: UITableViewController {
     }
     do_table_refresh();
   }
-
+  
+  func do_table_refresh()
+  {
+    dispatch_async(dispatch_get_main_queue(), {
+      self.tableView.reloadData()
+      return
+    })
+  }
 
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 1
