2107b852d28431ca7c1ae5ee066ad7fa0c633e8b
Daniel Golden <dangold@bergen.org>
2015-10-10 18:33:24 -0700

    some stuff is there

--- BitCoinViewer/FirstViewController.swift
+++ BitCoinViewer/FirstViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var TableData:Array< String > = Array < String >()
+
+class FirstViewController: UITableViewController {
 
   override func viewDidLoad() {
+    
+    let url = NSURL(string: "https://blockchain.info/ticker")
+
+    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+      if(data?.length > 0 && error == nil) {
+        let json = NSString(data: data!, encoding: NSASCIIStringEncoding)
+        parse_json(json);
+      }
+    }
+    
+    task.resume()
+    
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
@@ -19,7 +33,44 @@ class FirstViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func parse_json(data:NSString) {
+    let jsonData:NSData = data.dataUsingEncoding(NSUTF8StringEncoding)!
+    let json: AnyObject? = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions())
+    if let currencies = json as? NSArray {
+      for (var i = 0; i < currencies.count ; i++ ) {
+        if let currency_object = currencies[i] as? NSDictionary {
+          if let fifteenM = currencies["15m"] as? String {
+            if let last = currencies["last"] as? String {
+              if let buy = currencies["buy"] as? String {
+                if let sell = currencies["sell"] as? String {
+                  if let symbol = currencies["symbol"] as? String {
+                    
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    do_table_refresh();
+  }
 
 
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return TableData.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+    cell.textLabel?.text = TableData[indexPath.row]
+    return cell
+  }
+  
 }
 
--- BitCoinViewer/SecondViewController.swift
+++ BitCoinViewer/SecondViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class SecondViewController: UIViewController {
 
   override func viewDidLoad() {
+    let url = NSURL(string: "https://blockchain.info/ticker")
+    
+    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+      print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+    }
+    
+    task.resume()
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
